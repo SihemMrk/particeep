ed57d3527136f41caa8d4262e0c05b215f11c981
Sihem Meriki <sihemmeriki2@gmail.com>
2019-09-19 11:38:55 +0200

    cards

--- my-app/src/components/card.js
+++ my-app/src/components/card.js
@@ -3,27 +3,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { fetchData } from "../store";
 import { connect } from "react-redux";
-
-const useStyles = makeStyles({
-  card: {
-    minWidth: 275
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  }
-});
 
 class SimpleCard extends Component {
   //   constructor(props) {
@@ -33,39 +16,52 @@ class SimpleCard extends Component {
     await this.props.fetchData();
   }
   render() {
-    const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
+    const classes = makeStyles({
+      card: {
+        width: 50
+      },
+      bullet: {
+        display: "inline-block",
+        margin: "0 2px",
+        transform: "scale(0.8)"
+      },
+      title: {
+        fontSize: 14
+      },
+      pos: {
+        marginBottom: 12
+      }
+    });
+    // const bull = <span className={classes.bullet}>•</span>;
+    const data = this.props.dataSource;
 
     return (
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
-          >
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="h2">
-            be
-            {bull}
-            nev
-            {bull}o{bull}
-            lent
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            adjective
-          </Typography>
-          <Typography variant="body2" component="p">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+      <div>
+        {data.map(film => {
+          return (
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  {film.category}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {film.title}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                {/* <Button size="small">
+                 
+                
+                </Button> */}
+              </CardActions>
+            </Card>
+          );
+        })}
+      </div>
     );
   }
 }
